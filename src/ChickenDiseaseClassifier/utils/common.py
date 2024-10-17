c53c32357480df91a2c26e12d0c5e1a478c14e41
Mayank Kathane <maykat425@gmail.com>
2024-10-17 18:04:45 +0530

    relief

--- src/ChickenDiseaseClassifier/utils/common.py
+++ src/ChickenDiseaseClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from ChickenDiseaseClassifier import logger
+from cnnClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -11,16 +11,17 @@ from typing import Any
 import base64
 
 
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns ConfigBox
+    """reads yaml file and returns
 
     Args:
-        path_to_yaml (Path): path like input
-        
+        path_to_yaml (str): path like input
+
     Raises:
         ValueError: if yaml file is empty
-        e: empty yaml file
+        e: empty file
 
     Returns:
         ConfigBox: ConfigBox type
@@ -36,19 +37,20 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
     
 
+
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
-    
+
     Args:
-        path_to_directories (list): list of paths like input
-        ignore_log (bool, optional): ignore if multiple dirs are created. Defaults to False
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            loggger.info(f"created directory at: {path}")
-            
+            logger.info(f"created directory at: {path}")
+
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
@@ -58,26 +60,28 @@ def save_json(path: Path, data: dict):
         path (Path): path to json file
         data (dict): data to be saved in json file
     """
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         json.dump(data, f, indent=4)
-    logger.info(f"json data saved at: {path}")
-    
+
+    logger.info(f"json file saved at: {path}")
+
+
 
 
 @ensure_annotations
 def load_json(path: Path) -> ConfigBox:
     """load json files data
-    
+
     Args:
-        path: Path to the json file
-        
+        path (Path): path to json file
+
     Returns:
-        ConfigBox: data as class attribute instead of dictionary
-    """ 
+        ConfigBox: data as class attributes instead of dict
+    """
     with open(path) as f:
         content = json.load(f)
-        
-    logger.info(f"json file loaded successfully from: {path}")
+
+    logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
 
@@ -87,15 +91,28 @@ def save_bin(data: Any, path: Path):
 
     Args:
         data (Any): data to be saved as binary
-        path (Path): path to binary file 
+        path (Path): path to binary file
     """
     joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
-    
-    
+
 
 @ensure_annotations
-def load_bin(path: Path) -> str:
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
     """get size in KB
 
     Args:
@@ -113,8 +130,8 @@ def decodeImage(imgstring, fileName):
     with open(fileName, 'wb') as f:
         f.write(imgdata)
         f.close()
-        
+
 
 def encodeImageIntoBase64(croppedImagePath):
-    with open(croppedImagePath, 'rb') as f:
+    with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
